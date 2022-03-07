22f6b92b4c52882856e46416eda9ea6272a42884
Ryan Maxin <rsmaxin@uwaterloo.ca>
2022-03-07 13:02:21 -0500

    init2

---
+++ Sorting_visualizer/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer>
+        
+      </SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
