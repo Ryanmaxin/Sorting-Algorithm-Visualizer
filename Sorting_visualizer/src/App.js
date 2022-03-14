80a0424ffb4b9d547b82d134d71373b148444b93
Ryan Maxin <rsmaxin@uwaterloo.ca>
2022-03-14 10:21:10 -0400

    array built (no style)

--- Sorting_visualizer/src/App.js
+++ Sorting_visualizer/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer'
 
 function App() {
   return (
     <div className="App">
-      <SortingVisualizer>
-        
-      </SortingVisualizer>
+      <SortingVisualizer/>
     </div>
   );
 }
