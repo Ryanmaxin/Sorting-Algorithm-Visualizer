6f8ffbcf73119b0d4b7e985bc5524ed7fc8e3932
Ryan Maxin <rsmaxin@uwaterloo.ca>
2022-03-14 15:36:22 -0400

    Reformat + Bubble Sort

---
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -0,0 +1,43 @@
+import { useState,useEffect } from 'react';
+import './SortingVisualizer.css'
+import BubbleSort from '../SortingAlgorithms/BubbleSort'
+
+const SortingVisualizer = () => {
+    const [masterArray,setmasterArray] = useState([])
+    useEffect(()=>{
+        resetArray()
+    },[])
+    const resetArray = (()=> {
+        const array = []
+        for (let i = 0; i < 300; i++) {
+            array.push((Math.random()*(76)+5))
+        }
+        setmasterArray(array)
+    })
+    const handleBubbleSort = arr => {
+        const newArray = BubbleSort(arr)
+        console.log(newArray)
+        setmasterArray([...newArray])
+    }
+
+    return (
+        <div className="content">
+            <div className = "array-container">
+                {masterArray.map((element,i)=> (
+                <div 
+                className="array-bar" 
+                key={i}
+                style={{height: `${element}vh`}}
+                ></div>
+                ))}
+            </div>
+            <div className="options">
+                <button onClick={resetArray}>Generate New Array</button>
+                <button onClick={()=>{handleBubbleSort(masterArray)}}>Bubble Sort</button>
+            </div>
+        </div>
+
+    );
+}
+ 
+export default SortingVisualizer;
